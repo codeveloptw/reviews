26d8d7b6d93a0a15a31ca3ed1de7f870600add92
Daniel Chiu <lapulato@gmail.com>
2022-12-02 14:43:35 +0800

    add comment filter

--- reviews.py
+++ reviews.py
@@ -16,6 +16,11 @@ print(f'總長度 {total_len} ,平均長度 {total_len/len(data)}')
 
 # 篩選有 'good' 
 good = []
+#-------------------------
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+#-化簡為下面這一行------------------------
 good = [d for d in data if 'good' in d]
 print(len(good))
 print(f'含有 good 字串的第一筆資料 :\n {good[0]}')
